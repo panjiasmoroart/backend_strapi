ecf7262a1ad7a003cb7e3beeb50f9df28e31c1d7
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-10-06 22:56:05 +0700

    Backend - Explaining Strapi Endpoints

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -977,7 +977,6 @@ export interface PluginUsersPermissionsUser
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
@@ -991,6 +990,8 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    firstName: Schema.Attribute.String;
+    lastName: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
